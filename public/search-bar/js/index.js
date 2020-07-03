ce8fa87d7550acfae0522b7c04fcc8ab4b8d4fca
Mubariz Afzal <mubarizafzal@gmail.com>
2020-07-03 17:34:52 -0400

    added search bar widget

---
+++ public/search-bar/js/index.js
@@ -0,0 +1,8 @@
+console.log("ello");
+/*
+  TODO:
+    - select where to search through: definitions, synonyms, examples
+    - write JS functionality
+    - use of vendor code??
+
+*/
---
+++ public/search-bar/js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+console.log("ello");
+/*
+  TODO:
+    - select where to search through: definitions, synonyms, examples
+    - write JS functionality
+    - use of vendor code??
+
+*/
