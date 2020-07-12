99881c4189090a2bbc7b8fa33882b8db8aab298b
Mubariz Afzal <mubarizafzal@gmail.com>
2020-07-12 11:57:38 -0400

    improved build functionality

--- build.sh
+++ build.sh
@@ -11,10 +11,6 @@ if test -f "public/${arg}css/style.scss"
 then
   gnome-terminal --tab -e "sass --watch --sourcemap=none public/${arg}css/style.scss:public/${arg}css/main.css" 1> /dev/null &
 fi
-if test -f "public/${arg}js/index.js"
-then
-  gnome-terminal --tab -e "npx babel --watch public/${arg}js/index.js --out-file public/${arg}js/main.js --presets=@babel/preset-env" 1> /dev/null &
-fi
 
 if test $1
 then
