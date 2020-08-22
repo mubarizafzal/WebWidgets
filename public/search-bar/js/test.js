6b1f895428607328fcf03f87588b15dfea0827cd
Mubariz Afzal <mubarizafzal@gmail.com>
2020-08-22 13:37:56 -0400

    work on promises

--- public/search-bar/js/test.js
+++ public/search-bar/js/test.js
@@ -3,6 +3,17 @@ const MongoClient = require('mongodb').MongoClient;
 
 const dsn = 'mongodb://localhost:27017/temp'
 
+function insertMongodb (collection, data) {
+  const promisedInsert = [];
+
+  Object.keys(data).forEach( (key) => {
+    promisedInsert.push(
+      collection.insertOne({date: key, value: data[key]}) // return promise?
+    );
+  });
+  return Promise.all(promisedInsert);
+}
+
 MongoClient.connect(dsn, function (err, db) {
   if (err) throw err;
   console.log("connected successfully");
